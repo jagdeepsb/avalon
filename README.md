f25fe16623a512711a88acfa7198f5445105a12b
Jagdeep Bhatia <jagdeepsb150@gmail.com>
2024-04-23 20:55:19 -0400

    Set up avalon repo

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # avalon
 fuck
+
+# Install
+
+```bash
+conda create --name avalon python=3.10
+conda activate avalon
+pip install -r requirements.txt && pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='avalon_project',
+    version='1.0',
+    description='It\'s avalon bro',
+    author='Yo momma',
+    author_email='jagdeep@mit.edu',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    extras_require={
+    },
+)
