c1f61f1335b97d8242c388a4d737874b11446337
Ryan Yang <ryanyang@dhcp-10-31-134-201.dyn.MIT.EDU>
2024-05-07 05:28:28 -0400

    add get_action for random player

--- src/players/random_player.py
+++ src/players/random_player.py
@@ -6,11 +6,23 @@ from src.game.utils import (
 )
 from src.game.game_state import AvalonGameState
 from src.players.player import AvalonPlayer
+from src.game.utils import (
+    Role, QuestResult, RoundStage, GameStage
+)
     
 class RandomAvalonPlayer(AvalonPlayer):
     """
     Player that makes random decisions.
     """
+    def get_action(self, game_state: AvalonGameState):
+        if game_state.game_stage == GameStage.MERLIN_VOTE:
+            return random.choice(range(game_state.n_players))
+        elif game_state.round_stage == RoundStage.TEAM_PROPOSAL:
+            return random.sample(range(game_state.n_players), game_state.team_size)
+        elif game_state.round_stage == RoundStage.TEAM_VOTE:
+            return random.choice([TeamVote.APPROVE, TeamVote.REJECT])
+        elif game_state.round_stage == RoundStage.QUEST_VOTE:
+            return random.choice([QuestVote.SUCCESS, QuestVote.FAIL])
     
     def get_team_proposal(self, game_state: AvalonGameState) -> List[int]:
         return random.sample(range(game_state.n_players), game_state.team_size)
